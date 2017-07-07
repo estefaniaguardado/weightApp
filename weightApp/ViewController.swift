58a59dad4fa67fc52e88e85414e73e037f6283a2
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-07-07 13:40:19 +0200

    Init project

---
+++ weightApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weightApp
+//
+//  Created by Estefania Guardado on 07.07.17.
+//  Copyright © 2017 Estefania Guardado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
