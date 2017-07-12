412dc727e41ec976129a23adeb7a0e1a553f1fc6
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-07-12 11:04:57 +0200

    Create class and test files of Convertor Measures

---
+++ weightApp/Utilities/ConvertorMeasures.swift
@@ -0,0 +1,16 @@
+//
+//  ConvertorMeasures.swift
+//  weightApp
+//
+//  Created by Estefania Guardado on 12.07.17.
+//  Copyright © 2017 Estefania Guardado. All rights reserved.
+//
+
+import Foundation
+
+class ConvertorMeasure{
+
+    func feetToCentimeters(quantity: Float) -> Float {
+        return 30.48
+    }
+}
---
+++ weightAppTests/convertorMeasuresTest.swift
@@ -0,0 +1,30 @@
+//
+//  convertorMeasures.swift
+//  weightApp
+//
+//  Created by Estefania Guardado on 12.07.17.
+//  Copyright © 2017 Estefania Guardado. All rights reserved.
+//
+
+import XCTest
+@testable import weightApp
+
+class convertorMeasuresTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testOneFeetIsEqualToThirtyPointFourtyEightCentimeters() {
+        let convertor = ConvertorMeasure()
+        let result = convertor.feetToCentimeters(quantity: 1.0)
+        XCTAssertEqual(result, 30.48)
+    }
+    
+}
