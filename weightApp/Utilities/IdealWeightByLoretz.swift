9a4ad2d982e853ee9cccaba7e442f62c44c10af7
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-07-24 16:39:23 +0200

    Convert units of height

--- weightApp/Utilities/IdealWeightByLoretz.swift
+++ weightApp/Utilities/IdealWeightByLoretz.swift
@@ -17,27 +17,25 @@ class IdealWeightByLoretz{
     var gender: Gender
     var height: Float
     
+    let convertor = ConvertorMeasure()
+
     init(user: Gender, userHeight: Float) {
         gender = user
-        height = userHeight
+        height = convertor.metersToCentimeters(quantity: userHeight, decimals: 2)
     }
     
     func calculateIdealWeight() -> Float {
         
         if gender == Gender.female {
-            
             let formulaLoretzToFemale = (height - 100.0) - ((height - 150) / 2)
             return formulaLoretzToFemale
         }
         
         if gender == Gender.male {
-        
             let formulaLoretzToMale = (height - 100.0) - ((height - 150) / 4)
             return formulaLoretzToMale
         }
         
         return 0
-        
     }
-
 }
