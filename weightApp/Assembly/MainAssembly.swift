dd8c20044679a908e1d1c796f4840b1ad502e25f
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-08-10 16:16:27 +0200

    Configure Main Assembly

---
+++ weightApp/Assembly/MainAssembly.swift
@@ -0,0 +1,14 @@
+//
+//  MainAssembly.swift
+//  weightApp
+//
+//  Created by Estefania Guardado on 10.08.17.
+//  Copyright © 2017 Estefania Guardado. All rights reserved.
+//
+
+import Foundation
+import Typhoon
+
+class MainAssembly: TyphoonAssembly {
+    
+}
