b2842336eb84e989db0eb9df675f70375ee697fe
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-08-11 17:24:02 +0200

    Minor change

--- weightApp/Utilities/ConvertorMeasures.swift
+++ weightApp/Utilities/ConvertorMeasures.swift
@@ -8,13 +8,6 @@
 
 import Foundation
 
-extension Float {
-    func roundTo(places:Int) -> Float {
-        let divisor = pow(10.0, Float(places))
-        return (self * divisor).rounded() / divisor
-    }
-}
-
 class ConvertorMeasure{
 
     
