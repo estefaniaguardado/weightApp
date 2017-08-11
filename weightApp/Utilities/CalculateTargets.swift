1c020ed3db263509e6245648f07473ad5c1179d3
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-08-11 17:07:35 +0200

    Update CalculateTargets

---
+++ weightApp/Utilities/CalculateTargets.swift
@@ -0,0 +1,37 @@
+//
+//  CalculateTargets.swift
+//  weightApp
+//
+//  Created by Estefania Guardado on 11.08.17.
+//  Copyright © 2017 Estefania Guardado. All rights reserved.
+//
+
+import Foundation
+
+class CalculateTargets {
+    
+    let dateHandler = DatesHandler()
+    var idealWeightByLoretz: IdealWeightByLoretz!
+    let weightInGramsToLosePerWeek: Float = 0.453
+    
+    func kilosToWeeks(quantity: Int) -> Int {
+        let weeks = Float(quantity) / weightInGramsToLosePerWeek
+        return Int(weeks.rounded())
+    }
+    
+    func weeksToDays(quantity: Int) -> Int {
+        return quantity * 7
+    }
+    
+    func getTargetDate(currentDate: Date, kilos: Int) -> Date {
+        let resultWeeks = kilosToWeeks(quantity: kilos)
+        let totalDays = weeksToDays(quantity: resultWeeks)
+        let targetDate = dateHandler.addDaysToDate(date: currentDate, days: totalDays)
+        
+        return targetDate
+    }
+    
+    func getTargetWeight(gender: String, height: Int) -> Int {
+        return idealWeightByLoretz.calculateIdealWeight(userGender: gender, userHeight: height)
+    }
+}
